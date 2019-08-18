5b21d3b80ded903a8adfc83994ccdb91f440e13e
Tobias Braun <tobias.braun@rub.de>
2019-08-18 16:03:24 +0200

    Added input field and description with demo button

--- app/src/main/java/de/cotoami/cotoamishare/MainActivity.kt
+++ app/src/main/java/de/cotoami/cotoamishare/MainActivity.kt
@@ -2,11 +2,25 @@ package de.cotoami.cotoamishare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val submitButton = findViewById(R.id.button) as Button
+        val inputUrl = findViewById(R.id.editText) as EditText
+
+        submitButton.setOnClickListener {
+            Toast.makeText(
+                this@MainActivity,
+                inputUrl.text.toString(),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
