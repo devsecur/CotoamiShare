a3d6ca907df3d8416293bbd5ec7c8ccfd434a04e
Tobias Braun <tobias.braun@rub.de>
2019-08-19 23:49:13 +0200

    Bla

--- app/src/main/java/de/cotoami/cotoamishare/MainActivity.kt
+++ app/src/main/java/de/cotoami/cotoamishare/MainActivity.kt
@@ -1,26 +1,35 @@
 package de.cotoami.cotoamishare
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.activity_main)
 
-        val submitButton = findViewById(R.id.button) as Button
-        val inputUrl = findViewById(R.id.editText) as EditText
+        val submitButton = findViewById<Button>(R.id.button)
+        val inputUrl = findViewById<EditText>(R.id.editText)
 
         submitButton.setOnClickListener {
-            Toast.makeText(
-                this@MainActivity,
-                inputUrl.text.toString(),
-                Toast.LENGTH_SHORT
-            ).show()
+            val sharedPreference =  getSharedPreferences("Cotoami", Context.MODE_PRIVATE)
+
+            var editor = sharedPreference.edit().also {
+                it.putString("url",inputUrl.text.toString())
+                it.apply()
+            }
+
+            val intent = Intent(this, ShowActivity::class.java).also {
+                // start your next activity
+                startActivity(it)
+            }
         }
     }
 }
--- app/src/main/java/de/cotoami/cotoamishare/ShowActivity.kt
+++ app/src/main/java/de/cotoami/cotoamishare/ShowActivity.kt
@@ -1,12 +1,41 @@
 package de.cotoami.cotoamishare
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.CookieManager
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class ShowActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show)
+        val sharedPreference =  getSharedPreferences("Cotoami", Context.MODE_PRIVATE)
+
+        var mywebview = findViewById<WebView>(R.id.cotonamiView).also {
+            it.settings.javaScriptEnabled = true
+            it.settings.javaScriptCanOpenWindowsAutomatically = true
+            it.settings.domStorageEnabled = true
+        }
+        mywebview!!.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+
+            override fun onPageFinished(view: WebView, url: String) {
+                val cookies = CookieManager.getInstance().getCookie(url)
+                var editor = sharedPreference.edit().also {
+                    it.putString("cookie",cookies)
+                    it.apply()
+                }
+            }
+        }
+
+        val url = sharedPreference.getString("url", "https://cotoa.me")
+        mywebview.loadUrl(url)
     }
 }
