3f5c131ed1d9aa39318a672f79a5cb9d9a8a4829
Tobias Braun <tobias.braun@rub.de>
2019-08-19 23:48:52 +0200

    Included WebView for Cotoami mobile, added icons, extracted cookies

---
+++ app/src/main/java/de/cotoami/cotoamishare/ShowActivity.kt
@@ -0,0 +1,12 @@
+package de.cotoami.cotoamishare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show)
+    }
+}
